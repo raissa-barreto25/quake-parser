a5133ac302ca451bfbf4f7fadbff765e6f0c6d75
Raissa <rrbsiilva@gmail.com>
2026-03-01 12:52:56 -0300

    adicionei um  menu inicial e rota de busca por ID do jogo

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from analisador import GamesLog
 from ranking import gerar_ranking
 
@@ -24,8 +24,8 @@ def get_jogo(game_id):
 
     return jsonify(resultado)
 
-@app.route('/')
-def menu():
+@app.route('/games')
+def home():
     resultado = {}
 
     for i, jogo in enumerate(parser.jogos):
@@ -36,6 +36,34 @@ def menu():
         }
 
     return jsonify(resultado)
+
+@app.route('/')
+def menu():
+    return '''
+        <h2>Consultar jogo</h2>
+        <form action="/buscar" method="get">
+            <label>Digite o ID do jogo:</label>
+            <input type="number" name="game_id" required>
+            <button type="submit">Buscar</button>
+        </form>
+    '''
+
+@app.route('/buscar')
+def buscar():
+    from flask import request
+
+    game_id = int(request.args.get("game_id"))
+
+    if game_id - 1 >= len(parser.jogos) or game_id <= 0:
+        return {"erro": "Game não encontrado"}
+
+    jogo = parser.jogos[game_id - 1]
+
+    return {
+        "total_mortes": jogo.total_mortes,
+        "jogadores": jogo.jogadores,
+        "mortes": jogo.mortes
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
